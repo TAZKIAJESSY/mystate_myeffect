b6007ef8a2e396a8929496784da58e6b94f13222
tazkiajessy <jessy566eee@gmail.com>
2021-02-24 00:43:55 +0100

    some changes in articleList

--- src/components/Article/ArticleList.js
+++ src/components/Article/ArticleList.js
@@ -4,42 +4,42 @@ import axios from "axios";
 import ArticleCard from "./ArticleCard";
 
 function ArticleList() {
-  const [articles, set_articles] = useState([]);
+  //const [articles, set_articles] = useState([]);
 
-  // const [articles, set_articles] = useState([
-  //   {
-  //     id: 1,
-  //     title: "What is React all about?",
-  //     body:
-  //       "React is all about one-way data flow, the Virtual DOM, and transpiling JSX.",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "A lovely kid",
-  //     body: "In fact, a kid is also the name of a baby goat!",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "On placeholder image URLs",
-  //     body:
-  //       "So yeah, you won't be able to look these images up. They're placeholders",
-  //   },
-  // ]);
+  const [articles, set_articles] = useState([
+    {
+      id: 1,
+      title: "What is React all about?",
+      body:
+        "React is all about one-way data flow, the Virtual DOM, and transpiling JSX.",
+    },
+    {
+      id: 2,
+      title: "A lovely kid",
+      body: "In fact, a kid is also the name of a baby goat!",
+    },
+    {
+      id: 3,
+      title: "On placeholder image URLs",
+      body:
+        "So yeah, you won't be able to look these images up. They're placeholders",
+    },
+  ]);
 
-  useEffect(() => {
-    async function doSomeDataFetching() {
-      console.log("I'm gonna fetch some data!");
-      await Timeout.set(2000); // time in milliseconds!
-      console.log("Got back:");
-      const res = await axios.get(
-        "https://jsonplaceholder.typicode.com/posts?_limit=5"
-      );
-      // console.log(response);
-      // console.log("Articles fetched");
-      set_articles(res.data);
-    }
-    doSomeDataFetching();
-  }, []);
+  // useEffect(() => {
+  //   async function doSomeDataFetching() {
+  //     console.log("I'm gonna fetch some data!");
+  //     await Timeout.set(2000); // time in milliseconds!
+  //     console.log("Got back:");
+  //     const response = await axios.get(
+  //       "https://jsonplaceholder.typicode.com/posts?_limit=5"
+  //     );
+  //     // console.log(response);
+  //     // console.log("Articles fetched");
+  //     set_articles(response.data);
+  //   }
+  //   doSomeDataFetching();
+  // }, []);
 
   // console.log("WHAT IS ARTICLES?", articles);
 
@@ -53,8 +53,10 @@ function ArticleList() {
   //   return <div>Loading...</div>;
   // }
 
+  const [pressed, setpressed] = useState(true);
+
   const cardButton = () => {
-    set_articles(articles);
+    setpressed(!pressed);
   };
 
   return (
@@ -64,11 +66,18 @@ function ArticleList() {
         Clear notifications
       </button>{" "}
       <br />
-      {articles.map((ar, index) => {
-        return (
-          <ArticleCard key={index} id={ar.id} title={ar.title} body={ar.body} />
-        );
-      })}
+      {pressed
+        ? articles.map((ar, index) => {
+            return (
+              <ArticleCard
+                key={index}
+                id={ar.id}
+                title={ar.title}
+                body={ar.body}
+              />
+            );
+          })
+        : null}
     </div>
   );
 }
